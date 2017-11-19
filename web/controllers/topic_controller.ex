95c91994a12b10dd90e3613e952a984f6d0b35f6
Kamyl <kamillcybulski@gmail.com>
2017-11-19 10:14:51 +0100

    Add routes, helpers and templates for editing a topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -28,4 +28,27 @@ defmodule Discuss.TopicController do
 
     render conn, "index.html", topics: topics
   end
+
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+
+  def update(conn, %{"id" => topic_id, "topic" => new_topic}) do
+    prev_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(prev_topic, new_topic)
+
+    case Repo.update(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> 
+        render conn, "edit.html", changeset: changeset, topic: prev_topic
+    end
+  end
 end
