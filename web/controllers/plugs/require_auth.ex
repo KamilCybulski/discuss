b01e0adcb018d15e3f25a1b6906692d431f784b6
Kamyl <kamillcybulski@gmail.com>
2017-11-25 20:45:22 +0100

    Add require_auth plug

---
+++ web/controllers/plugs/require_auth.ex
@@ -0,0 +1,23 @@
+defmodule Discuss.Plugs.RequireAuth do
+  import Plug.Conn, only: [halt: 1]
+  import Phoenix.Controller only: [put_flash: 3, redirect: 2]
+  
+  alias Discuss.Router.Helpers
+
+  def init(_params) do end
+
+  def call(conn, _params) do
+    call(conn)
+  end
+
+  defp call(%{assigns: %{user: nil}} = conn) do
+    conn
+    |> put_flash(:error, "You must be logged in")
+    |> redirect(to: Helpers.topic_path(conn, :index))
+    |> halt()
+  end
+
+  defp call(%{assigns: %{user: user}} = conn) do
+    conn
+  end
+end
