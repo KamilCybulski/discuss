f8f4ceb55874432c59b5d710510e13c810fb6884
Kamyl <kamillcybulski@gmail.com>
2017-11-29 21:42:30 +0100

    Add handler for adding comments to DB

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -8,7 +8,13 @@ const createSocket = (topicId) => {
   channel.join()
     .receive("ok", resp => { console.log("Joined successfully", resp) })
     .receive("error", resp => { console.log("Unable to join", resp) });
-}
+
+  document.getElementById('add-comment-btn').addEventListener('click', () => {
+    const content = document.getElementById('comment-content').value
+
+    channel.push('comment:add', { content: content })
+  });
+};
 
 
 window.createSocket = createSocket;
