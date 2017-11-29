94d69aa57a8c43e253b2090bdf5a26f734877e9d
Kamyl <kamillcybulski@gmail.com>
2017-11-29 22:24:32 +0100

    Display comments in the individual topic page

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -1,12 +1,21 @@
 import { Socket } from "phoenix";
 
+const renderComments =  (comments) => {
+  const renderedComments = comments.map(c => (
+    `<li class="collection-item">
+      ${c.content}
+    </li>`
+  ));
+  document.getElementById('comment-list').innerHTML = renderedComments.join('');
+};
+
 const socket = new Socket("/socket", {params: {token: window.userToken}});
 socket.connect();
 
 const createSocket = (topicId) => {
   const channel = socket.channel(`comments:${topicId}`, {});
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => { renderComments(resp.comments); })
     .receive("error", resp => { console.log("Unable to join", resp) });
 
   document.getElementById('add-comment-btn').addEventListener('click', () => {
