3b2196881f787047171f2a6a896f097218c67837
Kamyl <kamillcybulski@gmail.com>
2017-11-29 22:45:37 +0100

    Add live updating comments section

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -1,13 +1,24 @@
 import { Socket } from "phoenix";
 
+const commentTemplate = (comment) => (
+  `<li class="collection-item">
+    ${comment.content}
+  </li>`
+);
+
+const extractComment = (event) => event.comment;
+
 const renderComments =  (comments) => {
-  const renderedComments = comments.map(c => (
-    `<li class="collection-item">
-      ${c.content}
-    </li>`
-  ));
+  const renderedComments = comments.map(commentTemplate);
   document.getElementById('comment-list').innerHTML = renderedComments.join('');
 };
+
+const renderComment = (comment) => {
+  const renderedComment = commentTemplate(comment);
+  document.getElementById('comment-list').innerHTML += renderedComment;
+};
+
+const handleNewComment = (event) => renderComment(extractComment(event));
 
 const socket = new Socket("/socket", {params: {token: window.userToken}});
 socket.connect();
@@ -17,6 +28,8 @@ const createSocket = (topicId) => {
   channel.join()
     .receive("ok", resp => { renderComments(resp.comments); })
     .receive("error", resp => { console.log("Unable to join", resp) });
+
+  channel.on(`comments:${topicId}:new`, handleNewComment)
 
   document.getElementById('add-comment-btn').addEventListener('click', () => {
     const content = document.getElementById('comment-content').value
